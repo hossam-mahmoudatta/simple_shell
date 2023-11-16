946a83eb1b9873664ff0c886ee1b97dd4934176a
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-11-16 02:19:43 +0200

    SAVING MORE FUNCTIONS

--- info.c
+++ info.c
@@ -58,19 +58,19 @@ void free_info(info_t *info, int all)
 	info->path = NULL;
 	if (all)
 	{
-		if (!info->cmd_buf)
-			free(info->arg);
+		if (!info->cmdBuffer)
+			free(info->argument);
 		if (info->env)
 			free_list(&(info->env));
 		if (info->history)
 			free_list(&(info->history));
 		if (info->alias)
 			free_list(&(info->alias));
-		ffree(info->environ);
-			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
-		if (info->readfd > 2)
-			close(info->readfd);
-		_putchar(BUF_FLUSH);
+		ffree(info->Environment);
+			info->Environment = NULL;
+		bfree((void **)info->cmdBuffer);
+		if (info->readFD > 2)
+			close(info->readFD);
+		_putchar(BUFFER_FLUSH);
 	}
 }
